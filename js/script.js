f55512ffc3a1344d67bcc3905aea37a6c2c27db3
Andrey <Vinogradow.Andrej@yandex.ru>
2022-03-04 20:06:53 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log('arr' + " - object");
+
+let incr = 10,
+	decr = 10;
+
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(2 + 2 * 2 != 8);
+
+const isChecked = true,
+	isClose = true;
+
+console.log(isChecked || isClose);
