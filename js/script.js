6dbe2070525d4e239c0f1a6f0e25054c8a7c15f8
Andrey <Vinogradow.Andrej@yandex.ru>
2022-03-09 20:24:52 +0300

    М2У18

--- js/script.js
+++ js/script.js
@@ -1,27 +1,76 @@
 "use strict";
 
-const str = "teSt";
+let num = 50;
+let numberOfFilms;
+// console.log(numberOfFilms);
 
-// console.log(str[2] = 'd');
+function start() {
+	numberOfFilms = +prompt('Сколько фильмов вы уже смотрели?', '');
 
-console.log(str.toUpperCase());
-console.log(str.toLocaleLowerCase());
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('Сколько фильмов вы уже смотрели?', '');
+	}
 
-let fruit = "Some fruit";
+}
+start();
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
 
-console.log(fruit.indexOf("i"));
 
-const logg = "Hello world";
+function writeYourGenres() {
+	for (let i = 1; i <= 3; i++) {
+		personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+	}
+}
+writeYourGenres();
 
-// console.log(logg.slice(6, 11));
 
-// console.log(logg.substring(6, 11));
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++) {
+		const a = prompt('Один из последних просмотренных фильмов', ''),
+			b = prompt('На сколько оцените его?', '');
+		if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+			personalMovieDB.movies[a] = b;
+			console.log('Done');
+		} else {
+			console.log('Error');
+			i--;
+		}
 
-console.log(logg.substr(6, 5));
+	}
+}
+//rememberMyFilms();
 
-const num = 12.2;
-console.log(Math.round(num));
+function detectPersonalLevel() {
+	for (let i = 0; i < 1; i++) {
+		if (personalMovieDB.count < 10) {
+			window.alert('Просмотрено довольно мало фильмов');
+			break;
+		}
+		if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+			window.alert('Вы классический зритель');
+			break;
+		}
+		if (personalMovieDB.count > 30) {
+			window.alert('Вы киноман');
+			break;
+		}
+		else {
+			window.alert('Произошла ошибка');
+			break;
+		}
+	}
+}
+//detectPersonalLevel();
 
-const test = "12.2px";
-console.log(parseInt(test));
-console.log(parseFloat(test));
+function showMyDB(hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB);
+	}
+}
+showMyDB(personalMovieDB.privat);
