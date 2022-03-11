325ff70386912ea7f55663b48cba185d0a679936
Andrey <Vinogradow.Andrej@yandex.ru>
2022-03-11 16:27:35 +0300

    M2Y28

--- js/script.js
+++ js/script.js
@@ -1,30 +1,23 @@
 "use strict";
 
-//let x = 5; alert(x++);
+const box = document.getElementById('box');
 
-//[] + false - null +true
-// console.log([] + false);
-// console.log(typeof ([] + false));
+console.log(box);
 
-//let y = 1; let x = y = 2; alert(x);
+const btns = document.getElementsByTagName('button');
 
-//console.log([] + 1 + 2);
+console.log(btns[1]);
 
-//alert("1"[0]);
+const circles = document.getElementsByClassName('circle');
 
-//console.log(2 && 1 && null && 0 && undefined);
+console.log(circles);
 
-//console.log(!!(1 && 2) === (1 && 2));
+const hearts = document.querySelectorAll('.heart');
 
-//Или запинается на правде && возвращает последнее значение
-// alert(null || 2 && 3 || 4);
+hearts.forEach(item => {
+	console.log(item);
+});
 
-// const a = [1, 2, 3];
-// const b = [1, 2, 3];
+const oneHeart = document.querySelector('.heart');
 
-// console.log(a == b);
-
-//alert("Infinity");
-//console.log("Ежик" > "яблоко");
-
-//console.log(0 || "" || 2 || undefined || true || false);
+console.log(oneHeart);
