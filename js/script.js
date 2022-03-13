751801c0a1e5944c886aed545c2c7ff94d72a131
Andrey <Vinogradow.Andrej@yandex.ru>
2022-03-13 15:12:05 +0300

    M2Y32

--- js/script.js
+++ js/script.js
@@ -1,42 +1,15 @@
 'use strict';
 
-const btns = document.querySelectorAll('button'),
-	overlay = document.querySelector('.overlay');
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
 
-// btn.onclick = function () {
-// 	alert('Клик');
-// };
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('#current').parentElement);
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
 
-// btn.onclick = function () {
-// 	alert('Клик2');
-// };
-
-
-let i = 0;
-const deleteElement = (e) => {
-	console.log(e.currentTarget);
-	console.log(e.type);
-	// i++;
-	// if (i == 1) {
-	// 	btn.removeEventListener('click', deleteElement);
-	// }
-};
-
-// btn.addEventListener('click', deleteElement);
-// overlay.addEventListener('click', deleteElement);
-
-btns.forEach(btn => {
-	btn.addEventListener('click', deleteElement, { once: true });
-});
-
-// btn.addEventListener('click', () => {
-// 	alert('Клик2');
-// });
-
-const link = document.querySelector('a');
-
-link.addEventListener('click', (event) => {
-	event.preventDefault();
-
-	console.log(event.target);
-});
+for (let node of document.body.childNodes) {
+	if (node.nodeName == '#text') {
+		continue;
+	}
+	console.log(node);
+}
