c139c2067376e2196ed4ae315cc0ba036281c498
Andrey <Vinogradow.Andrej@yandex.ru>
2022-03-13 14:53:15 +0300

    M2Y31

--- js/script.js
+++ js/script.js
@@ -1,48 +1,42 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
-const movieDB = {
-	movies: [
-		"Логан",
-		"Лига справедливости",
-		"Ла-ла лэнд",
-		"Одержимость",
-		"Скотт Пилигрим против..."
-	]
+const btns = document.querySelectorAll('button'),
+	overlay = document.querySelector('.overlay');
+
+// btn.onclick = function () {
+// 	alert('Клик');
+// };
+
+// btn.onclick = function () {
+// 	alert('Клик2');
+// };
+
+
+let i = 0;
+const deleteElement = (e) => {
+	console.log(e.currentTarget);
+	console.log(e.type);
+	// i++;
+	// if (i == 1) {
+	// 	btn.removeEventListener('click', deleteElement);
+	// }
 };
 
-const adv = document.querySelectorAll('.promo__adv img'),
-	promo = document.querySelector('.promo__bg'),
-	genre = promo.querySelector('.promo__genre'),
-	movieList = document.querySelector('.promo__interactive-list');
-console.log(adv);
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
 
-adv.forEach(item => {
-	item.remove();
+btns.forEach(btn => {
+	btn.addEventListener('click', deleteElement, { once: true });
 });
-genre.textContent = 'Драма';
-promo.style.cssText = 'background-image: url("img/bg.jpg")';
 
-movieList.innerHTML = "";
+// btn.addEventListener('click', () => {
+// 	alert('Клик2');
+// });
 
-movieDB.movies.sort();
+const link = document.querySelector('a');
 
-movieDB.movies.forEach((film, i) => {
-	movieList.innerHTML += `
-	<li class="promo__interactive-item">${i + 1}) ${film}<div class="delete"></div></li>`;
-	i++;
-	console.log(i);
+link.addEventListener('click', (event) => {
+	event.preventDefault();
+
+	console.log(event.target);
 });
