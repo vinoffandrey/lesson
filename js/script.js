63c1cd27827b219ccad04378950d1b39f4adf7e3
Andrey <Vinogradow.Andrej@yandex.ru>
2022-03-16 18:18:25 +0300

    M2Y45

--- js/script.js
+++ js/script.js
@@ -1,157 +1,22 @@
-window.addEventListener('DOMContentLoaded', function () {
+'use strict';
 
-	// Tabs
+function User(name, id) {
+	this.name = name;
+	this.id = id;
+	this.human = true;
+	this.hello = function () {
+		console.log(`Hello ${this.name}`);
+	};
+}
 
-	let tabs = document.querySelectorAll('.tabheader__item'),
-		tabsContent = document.querySelectorAll('.tabcontent'),
-		tabsParent = document.querySelector('.tabheader__items');
+User.prototype.exit = function () {
+	console.log(`Пользователь ${this.name} ушел`);
+};
 
-	function hideTabContent() {
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 30);
 
-		tabsContent.forEach(item => {
-			item.classList.add('hide');
-			item.classList.remove('show', 'fade');
-		});
-
-		tabs.forEach(item => {
-			item.classList.remove('tabheader__item_active');
-		});
-	}
-
-	function showTabContent(i = 0) {
-		tabsContent[i].classList.add('show', 'fade');
-		tabsContent[i].classList.remove('hide');
-		tabs[i].classList.add('tabheader__item_active');
-	}
-
-	hideTabContent();
-	showTabContent();
-
-	tabsParent.addEventListener('click', function (event) {
-		const target = event.target;
-		if (target && target.classList.contains('tabheader__item')) {
-			tabs.forEach((item, i) => {
-				if (target == item) {
-					hideTabContent();
-					showTabContent(i);
-				}
-			});
-		}
-	});
-
-	// Timer
-
-	const deadline = '2022-05-11';
-
-	function getTimeRemaining(endtime) {
-		const t = Date.parse(endtime) - Date.parse(new Date()),
-			days = Math.floor((t / (1000 * 60 * 60 * 24))),
-			seconds = Math.floor((t / 1000) % 60),
-			minutes = Math.floor((t / 1000 / 60) % 60),
-			hours = Math.floor((t / (1000 * 60 * 60) % 24));
-
-		return {
-			'total': t,
-			'days': days,
-			'hours': hours,
-			'minutes': minutes,
-			'seconds': seconds
-		};
-	}
-
-	function getZero(num) {
-		if (num >= 0 && num < 10) {
-			return '0' + num;
-		} else {
-			return num;
-		}
-	}
-
-	function setClock(selector, endtime) {
-
-		const timer = document.querySelector(selector),
-			days = timer.querySelector("#days"),
-			hours = timer.querySelector('#hours'),
-			minutes = timer.querySelector('#minutes'),
-			seconds = timer.querySelector('#seconds'),
-			timeInterval = setInterval(updateClock, 1000);
-
-		updateClock();
-
-		function updateClock() {
-			const t = getTimeRemaining(endtime);
-
-			days.innerHTML = getZero(t.days);
-			hours.innerHTML = getZero(t.hours);
-			minutes.innerHTML = getZero(t.minutes);
-			seconds.innerHTML = getZero(t.seconds);
-
-			if (t.total <= 0) {
-				clearInterval(timeInterval);
-			}
-		}
-	}
-
-	setClock('.timer', deadline);
-
-	//Modal мой вариант
-
-	const btns = document.querySelectorAll('[data-modal]'),
-		close = document.querySelector('[data-close]'),
-		modal = document.querySelector('.modal');
-
-	function openModal() {
-		modal.classList.toggle('_active');
-		document.body.style.overflow = "hidden";
-		clearInterval(modalTimerId);
-	}
-	btns.forEach(element => {
-		element.addEventListener('click', openModal);
-	});
-	function closeModal() {
-		modal.classList.remove('_active');
-		document.body.style.overflow = "";
-	}
-	close.addEventListener('click', closeModal);
-
-	modal.addEventListener('click', (e) => {
-		if (e.target === modal) {
-			closeModal();
-		}
-	});
-
-	document.addEventListener('keydown', (e) => {
-		if (e.code === 'Escape' && modal.classList.contains('_active')) {
-			closeModal();
-		}
-	});
-
-	const modalTimerId = setTimeout(openModal, 5000);
-
-	function showModalByScroll() {
-		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
-			openModal();
-			window.removeEventListener('scroll', showModalByScroll);
-		}
-	}
-	window.addEventListener('scroll', showModalByScroll);
-	//Modal другой вариант
-
-	// const modalTrigger = document.querySelectorAll('[data-modal]'),
-	// 	modalWindow = document.querySelector('.modal'),
-	// 	modalCloseBtn = document.querySelector('[data-close]');
-
-	// modalTrigger.forEach(btn => {
-	// 	btn.addEventListener('click', () => {
-	// 		modalWindow.classList.add('show');
-	// 		modalWindow.classList.remove('hide');
-	// 		document.body.style.overflow = "hidden";
-	// 	});
-	// 	modalCloseBtn.addEventListener('click', () => {
-	// 		modalWindow.classList.add('hide');
-	// 		modalWindow.classList.remove('show');
-	// 		document.body.style.overflow = "";
-	// 	});
-	// });
-
-});
+ivan.exit();
+console.log(ivan, alex);
+ivan.hello();
+alex.hello();
