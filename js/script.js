10146b2d9b063b8cad6a94c77e39191dfdbec0ce
Andrey <Vinogradow.Andrej@yandex.ru>
2022-03-16 15:37:23 +0300

    M2Y40

--- js/script.js
+++ js/script.js
@@ -1,37 +1,26 @@
 'use strict';
 
-const btn = document.querySelector('.btn');
-let timerId,
-	i = 0;
+const now = new Date('2020-05-01')
+// new Date.parse('2020-05-01');
 
-function myAnimation() {
-	const elem = document.querySelector('.box');
-	let pos = 0;
+console.log(now.setHours(40));
+console.log(now);
 
-	const id = setInterval(frame, 10);
-	function frame() {
-		if (pos == 300) {
-			clearInterval(id);
-		} else {
-			pos++;
-			elem.style.top = pos + "px";
-			elem.style.left = pos + "px";
-		}
-	}
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+	let some = i ** 3;
 }
+let end = new Date();
 
-btn.addEventListener('click', myAnimation);
-
-
-// function logger() {
-// 	if (i === 3) {
-// 		clearInterval(timerId);
-// 	}
-// 	console.log('text');
-// 	i++;
-// }
-
-// let id = setTimeout(function log() {
-// 	console.log('Hello!');
-// 	id = setTimeout(log, 500);
-// }, 500);
+console.log(`Цикл отработал за ${end - start} мс`);
