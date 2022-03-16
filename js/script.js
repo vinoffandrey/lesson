2717367bd04d3c211090f91a5960fd5a4c2d6755
Andrey <Vinogradow.Andrej@yandex.ru>
2022-03-16 15:09:40 +0300

    M2Y39

--- js/script.js
+++ js/script.js
@@ -1,38 +1,37 @@
 'use strict';
 
+const btn = document.querySelector('.btn');
+let timerId,
+	i = 0;
 
-window.addEventListener('DOMContentLoaded', () => {
-	const tabs = document.querySelectorAll('.tabheader__item'),
-		tabsContent = document.querySelectorAll('.tabcontent'),
-		tabsParent = document.querySelector('.tabheader__items');
+function myAnimation() {
+	const elem = document.querySelector('.box');
+	let pos = 0;
 
-	function hideTabContent() {
-		tabsContent.forEach(item => {
-			item.classList.add('hide');
-			item.classList.remove('show', "fade");
-		});
-		tabs.forEach(item => {
-			item.classList.remove('tabheader__item_active');
-		});
-	}
-	function showTabContent(i = 0) {
-		tabsContent[i].classList.add("show", "fade");
-		tabsContent[i].classList.remove("hide");
-		tabs[i].classList.add('tabheader__item_active');
-	}
-
-	hideTabContent();
-	showTabContent();
-
-	tabsParent.addEventListener('click', (event) => {
-		const target = event.target;
-		if (target && target.classList.contains('tabheader__item')) {
-			tabs.forEach((item, i) => {
-				if (target == item) {
-					hideTabContent();
-					showTabContent(i);
-				}
-			});
+	const id = setInterval(frame, 10);
+	function frame() {
+		if (pos == 300) {
+			clearInterval(id);
+		} else {
+			pos++;
+			elem.style.top = pos + "px";
+			elem.style.left = pos + "px";
 		}
-	});
-});
+	}
+}
+
+btn.addEventListener('click', myAnimation);
+
+
+// function logger() {
+// 	if (i === 3) {
+// 		clearInterval(timerId);
+// 	}
+// 	console.log('text');
+// 	i++;
+// }
+
+// let id = setTimeout(function log() {
+// 	console.log('Hello!');
+// 	id = setTimeout(log, 500);
+// }, 500);
