f392e613d5fb6b68e9758b108079f7a68a08fa2e
Andrey <Vinogradow.Andrej@yandex.ru>
2022-03-30 20:57:27 +0300

    M4Y68

--- js/some.js
+++ js/some.js
@@ -1,34 +1,17 @@
 'use strict';
 
-// new RegExp('pattern', 'flags');
-// /pattern/f
+const persone = {
+	name: 'Alex',
+	age: 25,
 
-// const ans = prompt('Введите ваше число');
+	get userAge() {
+		return this.age;
+	},
 
-// const reg = /\d/;
-// console.log(ans.match(reg));
+	set userAge(num) {
+		this.age = num;
+	}
+};
 
-const str = 'My name is R2D2';
-
-console.log(str.match(/\D/ig));
-
-
-// \D не числа
-// \W не буквы
-
-// \d все числа
-// \w все буквы
-// \s все пробелы
-
-// i - не зависимо от регистра
-// g - все значения
-// m - многострочный режим
-
-// console.log(ans.match(reg));
-
-// const pass = prompt('Password');
-
-// console.log(pass.replace(/./g, "*"));
-
-// console.log('12-34-56'.replace(/-/g, ':'));
-
+console.log(persone.userAge = 30);
+console.log(persone.userAge);
