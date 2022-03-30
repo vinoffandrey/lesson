21c0f5794fa73eaa188d58cae26fe6725af21285
Andrey <Vinogradow.Andrej@yandex.ru>
2022-03-30 16:04:13 +0300

    M2Y64 localStorage

--- js/some.js
+++ js/some.js
@@ -1,5 +1,49 @@
 'use strict';
 
+const checkbox = document.querySelector('#checkbox'),
+	form = document.querySelector('form'),
+	change = document.querySelector('#color');
+
+if (localStorage.getItem('isChecked')) {
+	checkbox.checked = true;
+}
+
+if (localStorage.getItem('bg') === 'changed') {
+	form.style.backgroundColor = 'red';
+}
+
+checkbox.addEventListener('change', () => {
+	localStorage.setItem('isChecked', true);
+});
+
+change.addEventListener('click', () => {
+	if (localStorage.getItem('bg') === 'changed') {
+		localStorage.getItem('bg');
+		form.style.backgroundColor = '#fff';
+	} else {
+		localStorage.setItem('bg', 'changed');
+		form.style.backgroundColor = 'red';
+	}
+});
+
+const persone = {
+	name: 'Alex',
+	age: 25
+};
+
+const serializedPersone = JSON.stringify(persone);
+localStorage.setItem('alex', serializedPersone);
+
+console.log(JSON.parse(localStorage.getItem('alex')));
+
+
+// localStorage.setItem('number', 5);
+
+// localStorage.removeItem('number');
+// localStorage.clear();
+
+// console.log(localStorage.getItem('number'));
+
 // filter
 
 // const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart'];
@@ -35,14 +79,14 @@
 // const res = arr.reduce((sum, current) => `${sum}, ${current}`);
 // console.log(res);
 
-const obj = {
-	ivan: 'persone',
-	ann: 'persone',
-	dog: 'animal',
-	cat: 'animal'
-};
+// const obj = {
+// 	ivan: 'persone',
+// 	ann: 'persone',
+// 	dog: 'animal',
+// 	cat: 'animal'
+// };
 
-const newArray = Object.entries(obj)
-	.filter(item => item[1] === 'persone')
-	.map(item => item[0]);
-console.log(newArray);
+// const newArray = Object.entries(obj)
+// 	.filter(item => item[1] === 'persone')
+// 	.map(item => item[0]);
+// console.log(newArray);
