bc7e3df7562acea90dccb8b8e8241487a4d53ac2
Andrey <Vinogradow.Andrej@yandex.ru>
2022-03-30 17:58:14 +0300

    M4Y65 регулярные выражения

--- js/some.js
+++ js/some.js
@@ -1,92 +1,34 @@
 'use strict';
 
-const checkbox = document.querySelector('#checkbox'),
-	form = document.querySelector('form'),
-	change = document.querySelector('#color');
+// new RegExp('pattern', 'flags');
+// /pattern/f
 
-if (localStorage.getItem('isChecked')) {
-	checkbox.checked = true;
-}
+// const ans = prompt('Введите ваше число');
 
-if (localStorage.getItem('bg') === 'changed') {
-	form.style.backgroundColor = 'red';
-}
+// const reg = /\d/;
+// console.log(ans.match(reg));
 
-checkbox.addEventListener('change', () => {
-	localStorage.setItem('isChecked', true);
-});
+const str = 'My name is R2D2';
 
-change.addEventListener('click', () => {
-	if (localStorage.getItem('bg') === 'changed') {
-		localStorage.getItem('bg');
-		form.style.backgroundColor = '#fff';
-	} else {
-		localStorage.setItem('bg', 'changed');
-		form.style.backgroundColor = 'red';
-	}
-});
-
-const persone = {
-	name: 'Alex',
-	age: 25
-};
-
-const serializedPersone = JSON.stringify(persone);
-localStorage.setItem('alex', serializedPersone);
-
-console.log(JSON.parse(localStorage.getItem('alex')));
+console.log(str.match(/\D/ig));
 
 
-// localStorage.setItem('number', 5);
+// \D не числа
+// \W не буквы
 
-// localStorage.removeItem('number');
-// localStorage.clear();
+// \d все числа
+// \w все буквы
+// \s все пробелы
 
-// console.log(localStorage.getItem('number'));
+// i - не зависимо от регистра
+// g - все значения
+// m - многострочный режим
 
-// filter
+// console.log(ans.match(reg));
 
-// const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart'];
+// const pass = prompt('Password');
 
-// const shortNames = names.filter(name => {
-// 	return name.length < 5;
-// });
-// console.log(shortNames);
+// console.log(pass.replace(/./g, "*"));
 
-// map
+// console.log('12-34-56'.replace(/-/g, ':'));
 
-// let answers = ['IvAn', 'AnnA', 'Hello'];
-
-// const result = answers.map(item => item.toLowerCase());
-// console.log(result);
-
-// every/some
-
-// const some = [4, 'qwq', 'dsadsadasd'];
-
-// console.log(some.some(item => typeof (item) === 'number'));
-// console.log(some.every(item => typeof (item) === 'number'));
-
-// reduce
-
-// const arr = [4, 5, 1, 3, 2, 6];
-
-// const res = arr.reduce((sum, current) => sum + current, 3);
-// console.log(res);
-
-// const arr = ['apple', 'pear', 'plum'];
-
-// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
-// console.log(res);
-
-// const obj = {
-// 	ivan: 'persone',
-// 	ann: 'persone',
-// 	dog: 'animal',
-// 	cat: 'animal'
-// };
-
-// const newArray = Object.entries(obj)
-// 	.filter(item => item[1] === 'persone')
-// 	.map(item => item[0]);
-// console.log(newArray);
