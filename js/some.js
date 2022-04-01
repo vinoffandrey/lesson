23997db1c28bfef62d54d5345e1c4cae53493e98
Andrey <Vinogradow.Andrej@yandex.ru>
2022-04-01 13:56:01 +0300

    M4Y69 Инкапсуляция классы

--- js/some.js
+++ js/some.js
@@ -1,17 +1,30 @@
 'use strict';
 
-const persone = {
-	name: 'Alex',
-	age: 25,
-
-	get userAge() {
-		return this.age;
-	},
-
-	set userAge(num) {
-		this.age = num;
+class User {
+	constructor(name, age) {
+		this.name = name;
+		this._age = age;
 	}
-};
 
-console.log(persone.userAge = 30);
-console.log(persone.userAge);
+	#surname = 'Petrychenko';
+
+	say = () => {
+		console.log(`Имя пользователя: ${this.name} ${this.#surname}, возраст: ${this._age}`);
+	}
+
+	get age() {
+		return this._age;
+	}
+
+	set age(age) {
+		if (typeof age === 'number' && age > 0 && age < 110) {
+			this._age = age;
+		} else {
+			console.log('Недопустимое значение');
+		}
+	}
+}
+
+const ivan = new User('Ivan', 27);
+console.log(ivan.surname);
+ivan.say();
