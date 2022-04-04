1147bad916e131a700a715914f8f095c0c10d6f2
Andrey <Vinogradow.Andrej@yandex.ru>
2022-04-04 17:04:27 +0300

    m4Y76 Babel, Core.js, pollyfil

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,14 @@
-import tabs from './js/modules/tabs';
-import modal from './js/modules/modal';
-import timer from './js/modules/timer';
-import cards from './js/modules/cards';
-import forms from './js/modules/forms';
-import slider from './js/modules/slider';
-import calc from './js/modules/calc';
-import { openModal } from './js/modules/modal';
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cards from './modules/cards';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import calc from './modules/calc';
+import { openModal } from './modules/modal';
 
 window.addEventListener('DOMContentLoaded', function () {
 
