8518ba94bb970eeaa89033cfcd43186c2a273e2e
Andrey <Vinogradow.Andrej@yandex.ru>
2022-04-04 15:59:35 +0300

    M4Y76 try catch

--- src/js/test.js
+++ src/js/test.js
@@ -1,10 +1,24 @@
-export let one = 1;
+'use strict';
 
-let two = 2;
-
-
-export { two };
-
-export default function sayHi() {
-	console.log('Hi!');
+try {
+	document.querySelector('#colors').addEventListener('click', () => {
+		console.log('click');
+	});
+} catch (e) {
+	console.log(e);
 }
+console.log('normal');
+
+// try {
+// 	console.log('Normal');
+// 	console.log(a);
+// 	console.log('result');
+// } catch (error) {
+// 	console.log(error.name);
+// 	console.log(error.message);
+// 	console.log(error.stack);
+// } finally {
+
+// }
+
+// console.log('Still normal');
